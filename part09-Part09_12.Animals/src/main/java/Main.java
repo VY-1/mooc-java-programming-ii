8ca463dac9221229c28b78f8ab1abec4113566af
VY <veasna.youphon@live.com>
2024-07-13 15:32:36 -0700

    complete animals project

--- part09-Part09_12.Animals/src/main/java/Main.java
+++ part09-Part09_12.Animals/src/main/java/Main.java
@@ -2,7 +2,28 @@
 public class Main {
 
     public static void main(String[] args) {
-        // you can test how your classes work here
+        // you can test how your classes work hereHerd herd = new Herd();
+        Dog dog = new Dog();
+        dog.bark();
+        dog.eat();
+
+        Dog fido = new Dog("Fido");
+        fido.bark();
+
+        Cat cat = new Cat();
+        cat.purr();
+        cat.eat();
+        
+        Cat garfield = new Cat("Garfield");
+        garfield.purr();
+
+        NoiseCapable dog2 = new Dog();
+        dog2.makeNoise();
+        
+        NoiseCapable cat2 = new Cat("Garfield");
+        cat2.makeNoise();
+        Cat c = (Cat) cat2;
+        c.purr();
 
     }
 
