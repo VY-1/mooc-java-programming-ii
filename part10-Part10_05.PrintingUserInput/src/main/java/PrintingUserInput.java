fda8293bf36e423abe974cb7178484659cc45b46
VY <veasna.youphon@live.com>
2024-07-13 23:33:53 -0700

    complete 10-5: PrintingUserInput

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -1,11 +1,27 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> stringList = new ArrayList<>();
+
+        while(true){
+            String input = scanner.nextLine();
+            if(input.isEmpty()){
+                break;
+            }
+
+            stringList.add(input);
+        }
+
+        stringList.stream()
+            .forEach(word -> System.out.println(word));
 
     }
+
+
 }
