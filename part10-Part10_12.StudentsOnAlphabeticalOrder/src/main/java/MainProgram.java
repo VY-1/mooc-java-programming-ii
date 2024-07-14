d53ec2024f33285a781aefedbce625a32fdadd15
VY <veasna.youphon@live.com>
2024-07-14 15:13:59 -0700

    Complete 10-12: StudentsOnAlphabeticalOrder

--- part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/MainProgram.java
+++ part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/MainProgram.java
@@ -1,10 +1,22 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainProgram {
 
     public static void main(String[] args) {
-//        Student first = new Student("jamo");
-//        Student second = new Student("jamo1");
-//        System.out.println(first.compareTo(second));
+       Student first = new Student("jamo");
+       Student second = new Student("jamo1");
+       System.out.println(first.compareTo(second));
+
+       List<Student> students = new ArrayList<>();
+       students.add(first);
+       students.add(second);
+       students.add(new Student("allen"));
+       students.add(new Student("david"));
+
+       System.out.println(students);
+       Collections.sort(students);
+       System.out.println(students);
     }
 }
--- part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
+++ part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/Student.java
@@ -1,6 +1,6 @@
 
 
-public class Student {
+public class Student implements Comparable<Student> {
 
     private String name;
 
@@ -10,6 +10,30 @@ public class Student {
 
     public String getName() {
         return name;
+    }
+
+    /*
+     *The exercise template includes the class Student, which has a name. Implement the Comparable interface in the Student class in a way, 
+     such that the compareTo method sorts the students in alphabetical order based on their names.
+
+    The name of the Student is a String, which implements Comparable itself. You may use its compareTo method when implementing the method for the Student class. 
+    Note that String.compareTo() also treats letters according to their size, while the compareToIgnoreCase method of the same class ignores the capitalization completely. 
+    You may use either of these methods in the exercise.
+     */
+    @Override
+    public int compareTo(Student student){
+        // if(this.name.equals(student.name)){
+        //     return 0;
+        // }
+        // else if(this.name.compareToIgnoreCase(student.name) > 1){
+        //     return 1;
+        // }
+        // else{
+        //     return -1;
+        // }
+
+        return this.getName().compareToIgnoreCase(student.getName());
+
     }
 
     @Override
