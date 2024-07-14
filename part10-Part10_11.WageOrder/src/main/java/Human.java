fb16eb8939d240dcf39f4ac90bbd61821a21d6cc
VY <veasna.youphon@live.com>
2024-07-14 14:48:17 -0700

    Complete 10-11: WageOrder

--- part10-Part10_11.WageOrder/src/main/java/Human.java
+++ part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -1,5 +1,5 @@
 
-public class Human {
+public class Human implements Comparable<Human>{
 
     private int wage;
     private String name;
@@ -17,6 +17,22 @@ public class Human {
         return wage;
     }
 
+    /*
+    You are provided with the class Human. A human has a name and wage information. 
+    Implement the interface Comparable in a way, such that the overridden compareTo 
+    method sorts the humans according to wage from largest to smallest salary.
+     */
+    @Override
+    public int compareTo(Human human){
+        if(this.wage == human.wage){
+            return 0;
+        }else if(this.wage > human.wage){
+            return -1;
+        }
+        else{
+            return 1;
+        }
+    }
 
     @Override
     public String toString() {
