fa51ff3d564797173e018e24e95e8195a970c70d
VY <veasna.youphon@live.com>
2024-07-13 23:44:02 -0700

    complete 10-6: LimitedNumbers

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -1,11 +1,28 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LimitedNumbers {
 
+    //Write a program that reads user input. When the user gives a negative number as an input, the input reading will be stopped. 
+    //After this, print all the numbers the user has given as input that are between 1 and 5.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numberList = new ArrayList<>();
+
+        while(true){
+            int input = scanner.nextInt();
+            if(input < 0){
+                break;
+            }
+
+            numberList.add(input);
+
+        }
+
+        numberList.stream().filter(number -> number<=5 && number>0).forEach(number -> System.out.println(number));;
 
     }
 }
