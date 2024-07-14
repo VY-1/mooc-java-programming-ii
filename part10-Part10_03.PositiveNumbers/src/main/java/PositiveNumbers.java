cac8bc04b3e4b199905778649fadf508f06d674b
VY <veasna.youphon@live.com>
2024-07-13 22:38:22 -0700

    complete 10-3: PositiveNumbers

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,4 +1,5 @@
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -9,6 +10,34 @@ public class PositiveNumbers {
         Scanner scanner = new Scanner(System.in);
         // test your method here
 
+        List<String> inputList = new ArrayList<>();
+
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while(true){
+            String input = scanner.nextLine();
+
+            if(input.equals("end")){
+                break;
+            }
+
+            inputList.add(input);
+
+            
+        }
+
+        //Convert List<String> to List<Integer> type
+        List<Integer> numberList = inputList.stream().map(Integer::valueOf).collect(Collectors.toList());
+
+        System.out.println("Positive Numbers: " + positive(numberList) );
+        
+
+
+
+    }
+
+    public static List<Integer> positive(List<Integer> numbers){
+        ArrayList<Integer> intList = numbers.stream().filter(number -> number >0).collect(Collectors.toCollection(ArrayList::new));
+        return intList;
     }
 
 }
