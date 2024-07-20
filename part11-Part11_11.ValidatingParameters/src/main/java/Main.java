aa7a087509349d2d96129b34887532a6ebb204c9
VY <veasna.youphon@live.com>
2024-07-20 13:50:06 -0700

    Complete 11-11: ValidatingParameters

---
+++ part11-Part11_11.ValidatingParameters/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import validating.Person;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Person person = new Person(null, 0);
+
+        //System.out.println(person);
+
+        Person person2 = new Person("Dave", 21);
+        System.out.println(person2.getName());
+    }
+}
--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -6,6 +6,9 @@ public class Person {
     private int age;
 
     public Person(String name, int age) {
+        if(name == null|| name.isEmpty() || name.isBlank() || name.length()> 40 || age<0 || age>120){
+            throw new IllegalArgumentException("Name should not be empty and age shuold be between 0-120");
+        }
 
         this.name = name;
         this.age = age;
