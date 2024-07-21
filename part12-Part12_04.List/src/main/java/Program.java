61e778dac45f7d649854eb1473d159b46da32214
VY <veasna.youphon@live.com>
2024-07-21 13:11:01 -0700

    Complete 12-4: List

--- part12-Part12_04.List/src/main/java/Program.java
+++ part12-Part12_04.List/src/main/java/Program.java
@@ -7,6 +7,22 @@ public class Program {
         Scanner scan = new Scanner(System.in);
         // test your class here
 
+        List<String> myList = new List<>();
+        System.out.println(myList.contains("hello"));
+        myList.add("hello");
+        myList.add("world");
+        myList.add("How you doing");
+        System.out.println(myList.contains("hello"));
+        int index = myList.indexOfValue("hello");
+        System.out.println(index);
+        System.out.println(myList.value(index));
+        myList.remove("hello");
+        System.out.println(myList.contains("hello"));
+
+        for(int i = 0; i < myList.size(); i++){
+            System.out.println(myList.value(i));
+        }
+
     }
 
 }
