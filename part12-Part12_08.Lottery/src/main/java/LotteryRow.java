781d30c55ab13bd1be4e324811593dc6c8885127
VY <veasna.youphon@live.com>
2024-07-22 14:29:59 -0700

    Complete 12-8: LotteryRow

--- part12-Part12_08.Lottery/src/main/java/LotteryRow.java
+++ part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -20,11 +20,41 @@ public class LotteryRow {
         this.numbers = new ArrayList<>();
         // Implement the random number generation here
         // the method containsNumber is probably useful
+        Random randomGenerator = new Random();
+        int count =0;
+        while(count< 7){
+            int number = randomGenerator.nextInt(40)+1;
+            if(this.containsNumber(number)){
+                continue;
+            }
+
+            this.numbers.add(number);
+            count++;
+
+        }
+
+
     }
 
     public boolean containsNumber(int number) {
         // Check here whether the number is among the drawn numbers
+        
+        return this.numbers.contains(number);
+    }
+
+    public boolean equals(Object other){
+        if(this == other){
+            return true;
+        }
+        if(!(other instanceof LotteryRow)){
+            return false;
+        }
+        LotteryRow comparedLotteryRow = (LotteryRow) other;
+        if(this.numbers == comparedLotteryRow.numbers){
+            return true;
+        }
         return false;
+        
     }
 }
-
+
