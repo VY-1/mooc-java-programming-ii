587557fa1d8d3309e7febe128418a7a6c24515c6
VY <veasna.youphon@live.com>
2024-07-22 15:38:14 -0700

    Complete 12-9: ArrayAsAString

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -3,6 +3,29 @@ public class Program {
 
     public static void main(String[] args) {
         // Test your method here
+
+        int rows = 2;
+        int columns = 3;
+        int[][] matrix = new int[rows][columns];
+        matrix[0][1] = 5;
+        matrix[1][0] = 3;
+        matrix[1][2] = 7;
+        System.out.println(arrayAsString(matrix));
+    }
+
+    public static String arrayAsString(int[][] array){
+        
+        int[][] newArray = array;
+        String stringRepresent = "";
+        for(int i =0; i < newArray.length; i++){
+            for(int j = 0; j < newArray[i].length; j++){
+
+                stringRepresent += newArray[i][j];
+                
+            }
+            stringRepresent +="\n";
+        }
+        return stringRepresent;
     }
 
 }
